a1b487875af688e89c6c4bfe7b2e40f67937979b
Vladimir Milenko <v.milenko@innosoft.pro>
2016-12-14 14:16:09 +0300

    Locale fix #3

--- src/Stores/LogsStore.js
+++ src/Stores/LogsStore.js
@@ -5,7 +5,9 @@ import {observable, reaction} from 'mobx';
 import * as axios from "axios";
 import * as moment from "moment";
 import ru from 'moment/locale/ru'
-moment.locale('ru',ru);
+
+moment.locale('ru');
+
 class LogsStore {
 
     @observable cameraState = {
